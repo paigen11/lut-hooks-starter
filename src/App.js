4ca421d01acd2ccfaf523321c79b2d0cd7c74127
Paige Niedringhaus <paigen11@gmail.com>
2019-04-02 14:27:31 -0400

    firing up the first useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+// useState can only be used in function based components
 const App = () => {
+  // the recipe for useState always
+  // const [value, setValue] = useState(initialState);
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <h3>{name}</h3>
+      <input type="text" onChange={e => setName(e.target.value)} value={name} />
     </div>
   );
 };
