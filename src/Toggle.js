004046201b7d5ec440bd90548677ae136f83b0d2
Paige Niedringhaus <paigen11@gmail.com>
2019-04-30 16:02:13 -0400

    more complex hook

--- src/Toggle.js
+++ src/Toggle.js
@@ -14,7 +14,7 @@ const Toggle = () => {
   return (
     <div>
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
-      {isToggled && <DishForm />}
+      {isToggled && <DishForm setToggle={setToggle} />}
     </div>
   );
 };
